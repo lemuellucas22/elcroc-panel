dd61d74abdd79388aaf74e1189bbae548c80c657
lemuellucas22 <lemuell602@gmail.com>
2026-03-02 18:28:52 -0300

    Atualizar o page.tsx
    Create admin dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,8 @@
-export default function Dashboard() {
+export default function AdminPage() {
   return (
     <div style={{ padding: 40 }}>
-      <h1>Dashboard – El croc foods</h1>
-      <p>Você está logado com sucesso.</p>
+      <h1>Painel Administrativo – El Croc Foods</h1>
+      <p>Aqui você poderá gerenciar usuários.</p>
     </div>
   )
 }
