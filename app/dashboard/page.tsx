d60a14dc641ecb922526f1c8d74eb31d8c4ca3d1
lemuellucas22 <lemuell602@gmail.com>
2026-03-02 17:53:58 -0300

    Criar app/dashboard/page.tsx
    Criando dashboard inicial

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+export default function Dashboard() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Dashboard – El croc foods</h1>
+      <p>Você está logado com sucesso.</p>
+    </div>
+  )
+}
