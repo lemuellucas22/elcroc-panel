622cbf4218d87c01b8a195c1fa7cb4f37efa01ca
lemuellucas22 <lemuell602@gmail.com>
2026-03-02 18:59:25 -0300

    Atualizar o page.tsx
    protecr dashboard route

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,22 @@
-export default function AdminPage() {
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+import { users } from '@/app/lib/users'
+
+export default async function Dashboard() {
+  const cookieStore = await cookies()
+  const session = cookieStore.get('session')?.value
+
+  const user = users.find(u => u.username === session)
+
+  if (!user) {
+    redirect('/')
+  }
+
   return (
     <div style={{ padding: 40 }}>
-      <h1>Painel Administrativo – El Croc Foods</h1>
-      <p>Aqui você poderá gerenciar usuários.</p>
+      <h1>Dashboard – El croc foods</h1>
+      <p>Bem-vindo, {user.username}</p>
+      <p>Seu cargo: {user.role}</p>
     </div>
   )
 }
