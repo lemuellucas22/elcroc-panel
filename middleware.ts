a5b1f6bcf3af266130716282866be8f2cede3a15
lemuellucas22 <lemuell602@gmail.com>
2026-03-02 18:02:25 -0300

    Atualizar o middleware.ts
    middwale com expiração auto

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,28 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
+import { users } from '@/app/lib/users'
 
 export function middleware(req: NextRequest) {
   const session = req.cookies.get('session')
 
-  if (!session && req.nextUrl.pathname.startsWith('/dashboard')) {
+  if (!session) {
+    if (req.nextUrl.pathname.startsWith('/dashboard')) {
+      return NextResponse.redirect(new URL('/login', req.url))
+    }
+    return
+  }
+
+  const user = users.find(u => u.username === session.value)
+
+  if (!user) {
     return NextResponse.redirect(new URL('/login', req.url))
+  }
+
+  if (user.expiresAt && Date.now() > user.expiresAt) {
+    const res = NextResponse.redirect(
+      new URL('/login?expired=true', req.url)
+    )
+    res.cookies.delete('session')
+    return res
   }
 }
