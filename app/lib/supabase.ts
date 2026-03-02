374b334f85378177daf0cc5216b1582456e1d3d8
lemuellucas22 <lemuell602@gmail.com>
2026-03-02 19:51:15 -0300

    Atualizar o supabase.ts
    db

--- app/lib/supabase.ts
+++ app/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.SUPABASE_URL!
-const supabaseKey = process.env.SUPABASE_ANON_KEY!
+const SUPABASE_URL = 'https://Elcroc-panel.supabase.co'
+const SUPABASE_ANON_KEY = 'sb_publishable_7flcB_VqT45HW67kHVgYcQ_KUpRWk65'
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
